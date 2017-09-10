6fb6e1b4ab11fc365b63653b4ba48aa92abf041b
Ryan Scott <ryanscott161@gmail.com>
2017-09-11 11:25:59 +1200

    Correctly accept query-params helper
    closes #9

--- addon/helpers/transition-to.js
+++ addon/helpers/transition-to.js
@@ -5,14 +5,27 @@ export default Helper.extend({
   router: computed(function() {
     return getOwner(this).lookup('router:main');
   }).readOnly(),
+  
   compute([routeName, ...params]) {
     let router = get(this, 'router');
     assert('[ember-transition-helper] Unable to lookup router', router);
     return function(...invocationArgs) {
       let args = params.concat(invocationArgs);
       let transitionArgs = params.length ? [routeName, ...params] : [routeName];
+      transitionArgs = this._transformQueryParams(transitionArgs);
       router.transitionTo.apply(router, transitionArgs);
       return args;
     };
+  },
+  
+    // Transforms query-params template helper to what transitonTo expects
+  _transformQueryParams(transitionArgs) {
+    return transitionArgs.map((arg) => {
+      if (arg.isQueryParams) {
+        return { queryParams: arg.values };
+      } else {
+        return arg;
+      }
+    });
   }
 });
